acf1b8fdcb23c314032dcd51ea49aabc363904ab
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-09-15 20:36:55 +0200

    get + variable

--- src/main/java/UCNC/com/Mlyn/HelloMlynController.java
+++ src/main/java/UCNC/com/Mlyn/HelloMlynController.java
@@ -1,6 +1,7 @@
 package UCNC.com.Mlyn;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloMlynController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/witaj")
-    public String helloMlyn() {
-        return "Witaj w Młynie";
+    @RequestMapping(method = RequestMethod.GET, path = "/witaj/{name}")
+    public String helloMlyn(@PathVariable String name) {
+        return String.format("Witaj %s",name);
     }
 }
